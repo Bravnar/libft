1f997e8c753496b45e6bfd79c7f7524441d0ea75
Bravnar <130234241+Bravnar@users.noreply.github.com>
2023-09-12 13:50:34 +0000

    Adding ft_memchr.c CAREFUL IT's still wrong

---
+++ libft/ft_memchr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 13:06:25 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/12 13:48:03 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (n)
+	{
+		if (((unsigned char *)s)[n] == ((unsigned char)c))
+			return (s);
+		n--;
+	}
+	return (NULL);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str[] = "Hello, world!";
+    int search_char = 'o';
+    size_t n = ft_strlen(str);
+	
+    void *ft_result = ft_memchr(str, search_char, n);
+    if (ft_result != NULL) {
+        printf("ft_memchr: Found '%c' at position %ld\n", search_char, (char *)ft_result - str);
+    } else {
+        printf("ft_memchr: '%c' not found\n", search_char);
+    }
+
+    void *mem_result = memchr(str, search_char, n);
+    if (mem_result != NULL) {
+        printf("memchr: Found '%c' at position %ld\n", search_char, (char *)mem_result - str);
+    } else {
+        printf("memchr: '%c' not found\n", search_char);
+    }
+
+    return 0;
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 20:58:47 by bravnar           #+#    #+#             */
-/*   Updated: 2023/09/12 12:49:26 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/12 13:48:47 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ int		ft_tolower(int c);
 char 	*ft_strchr(const char *s, int c);
 char 	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
