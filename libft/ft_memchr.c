fdf164ebf0c95c6e1e3f7c9babc0cc4b16cb1aa5
Bravnar <stan.muravyev@bluewin.ch>
2023-09-23 13:26:43 +0200

    fixing memchr and strncmp

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bravnar <bravnar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/12 13:06:25 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/23 12:52:26 by bravnar          ###   ########.fr       */
+/*   Created: 2023/09/23 13:17:28 by bravnar           #+#    #+#             */
+/*   Updated: 2023/09/23 13:25:42 by bravnar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (n)
+	size_t	i;
+
+	i = 0;
+	while (n > i)
 	{
-		if (((unsigned char *)s)[n] == ((unsigned char)c))
-			return (s);
-		n--;
+		if (((unsigned char *)s)[i] == ((unsigned char)c))
+			return ((void *)s + i);
+		i++;
 	}
 	return (NULL);
 }
 
-/* #include <stdio.h>
-#include <string.h>
+/* #include <string.h>
+#include <stdio.h>
 
 int main() 
 {
@@ -33,17 +36,20 @@ int main()
     size_t n = ft_strlen(str);
 	
     void *ft_result = ft_memchr(str, search_char, n);
-    if (ft_result != NULL) {
-        printf("ft_memchr: Found '%c' at position %ld\n", \
-            , (char *)ft_result - str);
-    } else {
+    if (ft_result != NULL) 
+	{
+        printf("ft_memchr: Found '%c' at position %ld\n", 
+			search_char, (char *)ft_result - str);
+    } 
+	else 
+	{
         printf("ft_memchr: '%c' not found\n", search_char);
     }
 
     void *mem_result = memchr(str, search_char, n);
     if (mem_result != NULL) {
         printf("memchr: Found '%c' at position %ld\n", 
-            search_char, (char *)mem_result - str);
+			search_char, (char *)mem_result - str);
     } else {
         printf("memchr: '%c' not found\n", search_char);
     }
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bravnar <bravnar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 12:02:57 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/23 12:52:52 by bravnar          ###   ########.fr       */
+/*   Updated: 2023/09/23 13:14:53 by bravnar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return (0);
+	if (s1[i] || s2[i])
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 /* #include <stdio.h>
