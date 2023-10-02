7c12a2eecd0a616eb437720b933f254c5270520e
bravnar <stan.muravyev@bluewin.ch>
2023-10-02 17:41:54 +0200

    Pushing putendl_fd and updates to makefile and header

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 20:58:47 by bravnar           #+#    #+#             */
-/*   Updated: 2023/10/02 16:41:08 by smuravyev        ###   ########.fr       */
+/*   Updated: 2023/10/02 17:34:28 by smuravyev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
