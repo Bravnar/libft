1f31c5772d055a826080805a6a53662f8cca79f3
bravnar <stan.muravyev@bluewin.ch>
2023-10-02 17:09:39 +0200

    Pushing calloc and strdup (careful maybe problem with strdup)

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:35:14 by smuravyev         #+#    #+#             */
-/*   Updated: 2023/10/02 16:39:41 by smuravyev        ###   ########.fr       */
+/*   Updated: 2023/10/02 17:00:19 by smuravyev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	
+	void	*cset;
+	size_t	total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total_size = nmemb * size;
+	cset = malloc(total_size);
+	if (cset != NULL)
+		ft_bzero(cset, total_size);
+	else
+		return (NULL);
 }
--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:34:48 by smuravyev         #+#    #+#             */
-/*   Updated: 2023/10/02 16:38:20 by smuravyev        ###   ########.fr       */
+/*   Updated: 2023/10/02 17:04:02 by smuravyev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strdup(const char *s)
 {
-	
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	new_str = malloc(sizeof(char) * ft_strlen(s));
+	if (!new_str)
+		return (NULL);
+	while (s[i])
+	{
+		new_str[i] = s[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
