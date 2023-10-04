a6fc0057c6eabac0b3852d890d57694b0dc7a105
bravnar <stan.muravyev@bluewin.ch>
2023-10-04 19:14:11 +0200

    Pushing ft_substr.c, no test

---
+++ libft/ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 14:46:55 by smuravyev         #+#    #+#             */
+/*   Updated: 2023/10/04 19:11:43 by smuravyev        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int	i;
+	char *substr;
+
+	i = 0;
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	while (s[i])
+	{
+		if (!ft_strncmp(s[i], start, len))
+		{
+			while (s[i] && i < len)
+			{
+				substr[i] = s[i];
+				i++;
+			}
+			return (substr);
+		}
+		i++;
+	}
+	return (NULL);
+}
