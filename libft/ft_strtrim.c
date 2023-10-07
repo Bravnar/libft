a820aaeac1c6ae7b2b28b9e2bafecbf4ae62ba54
bravnar <stan.muravyev@bluewin.ch>
2023-10-07 12:13:20 +0200

    pushing ft_strtrim.c, really not sure...

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,17 +6,16 @@
 /*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:43:25 by smuravyev         #+#    #+#             */
-/*   Updated: 2023/10/05 22:41:36 by smuravyev        ###   ########.fr       */
+/*   Updated: 2023/10/07 12:09:26 by smuravyev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_in_set(char c, const char *set)
+static int	is_in_set(const char c, const char *set)
 {
 	int	i;
 
-	i = 0;
 	while (set[i])
 	{
 		if (c == set[i])
@@ -26,50 +25,58 @@ static int	is_in_set(char c, const char *set)
 	return (0);
 }
 
-static int	in_set_count(const char *s1, const char *set)
+static size_t	def_start(const char *s1, const char *set, size_t start)
 {
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (s1[i])
+	while (*s1)
 	{
-		if (is_in_set(s1[i], set))
-			count++;
-		i++;
+		if (is_in_set(s1[start], set))
+			start++;
+		else
+			return (start);
 	}
-	return (count);
+	return (start);
+}
+
+static size_t	def_end(const char *s1, const char *set, size_t end)
+{
+	while (end > 0)
+	{
+		if (is_in_set(s1[end], set))
+			end--;
+		else
+			return (end);
+	}
+	return (end);
 }
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	size_t		count;
-	int		i;
+	size_t	i;
+	size_t	len;
+	size_t	start;
+	size_t	end;
 	char	*trimmed;
 
-	i = 0;
-	count = in_set_count(s1, set);
-	trimmed = (char *)malloc(sizeof(char) * (ft_strlen(s1) - count + 1));
+	start = def_start(s1, set, 0);
+	end = def_end(s1, set, ft_strlen(s1) - 1);
+	len = end - start;
+	trimmed = (char *)malloc(sizeof(char) *(len + 1));
 	if (!trimmed)
 		return (NULL);
-	while (*s1)
-	{
-		while (is_in_set(*s1, set))
-			s1++;
-		if (*s1)
-			trimmed[i++] = *s1;
-		s1++;
-	}
+	i = 0;
+	while (i <= len && start <= end)
+		trimmed[i++] = s1[start++];
 	trimmed[i] = '\0';
 	return (trimmed);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 
-int	main()
+int	main(void)
 {
-	char	*s1 = "   Hello, World   ";
-	char	*trimmed = ft_strtrim(s1, " ");
-	printf("Trimmed string: %s\n", trimmed);
+	char	*str = "pp01234567890pp";
+	char	*set = "p";
+	char	*trimmed_str = ft_strtrim(str, set);
+	printf("The trimmed string is: %s\n", trimmed_str);
 }
+ */
