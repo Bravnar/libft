699700d493ef669226597f71a11a918a75dbf584
bravnar <stan.muravyev@bluewin.ch>
2023-10-07 12:33:27 +0200

    Small change substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: smuravyev <smuravyev@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:46:55 by smuravyev         #+#    #+#             */
-/*   Updated: 2023/10/05 13:08:57 by smuravyev        ###   ########.fr       */
+/*   Updated: 2023/10/07 12:32:50 by smuravyev        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 
 	i = 0;
 	j = 0;
-	substr = (char *)malloc(sizeof(*s) * (len + 1));
+	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (NULL);
 	while (s[i])
