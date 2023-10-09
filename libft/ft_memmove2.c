4b16af0b429db36a4da37a0d1c3ba27ab73e3a70
Stanislav Muravyev <smuravye@c1r16s1.42lausanne.ch>
2023-10-09 15:27:53 +0200

    changing memmove

--- libft/ft_memmove2.c
+++ libft/ft_memmove2.c
@@ -1,34 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memmove2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/14 22:04:21 by bravnar           #+#    #+#             */
-/*   Updated: 2023/10/09 15:20:10 by smuravye         ###   ########.fr       */
+/*   Created: 2023/10/09 15:18:12 by smuravye          #+#    #+#             */
+/*   Updated: 2023/10/09 15:26:16 by smuravye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h" //get back to
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t count)
 {
-	unsigned char	*temp;
-	size_t			on;
+	size_t	i;
 
-	on = n;
 	if (!dest && !src)
 		return (NULL);
-	while (n)
+	i = 0;
+	if (src > dest)
 	{
-		n--;
-		temp[n] = ((unsigned char *)src)[n];
+		while (i < n)
+		{
+			((char *)dest)[i] = ((char *)src)[i];
+			i++;
+		}
 	}
-	while (on)
+	else
 	{
-		on--;
-		((unsigned char *)dest)[on] = temp[on];
+		while (n)
+		{
+			n--;
+			((char *)dest)[n] = ((char *)src)[i];
+		}
 	}
 	return (dest);
 }
