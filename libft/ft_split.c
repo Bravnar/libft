4d9a7d1afd137cb3ba177ac8ba8be1775f91332c
Stanislav Muravyev <smuravye@c1r16s1.42lausanne.ch>
2023-10-09 17:32:37 +0200

    Pushing ft_split

---
+++ libft/ft_split.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:14:09 by smuravye          #+#    #+#             */
+/*   Updated: 2023/10/09 17:15:32 by smuravye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i])
+	{
+		while (s[i] && (s[i] == c))
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && (s[i] != c))
+			i++;
+	}
+	return (count);
+}
+
+static size_t	word_len(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && (s[i] != c))
+		i++;
+	return (i);
+}
+
+static char	*malloc_word(char const *s, char c)
+{
+	size_t	word_length;
+	size_t	i;
+	char	*word;
+
+	i = 0;
+	word_length = word_len(s, c);
+	word = (char *)malloc(sizeof(char) * (word_length + 1));
+	if (!word)
+		return (NULL);
+	while (i < word_length)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split_arr;
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	split_arr = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!split_arr)
+		return (NULL);
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			split_arr[i++] = malloc_word(s, c);
+		}
+		while (*s && *s != c)
+			s++;
+	}
+	split_arr[i] = NULL;
+	return (split_arr);
+}
+
+/* #include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	int		i;
+	char	**new_arr;
+
+	i = 0;
+	if (ac < 2)
+		return (0);
+	new_arr = ft_split(av[1], ' ');
+	while (new_arr[i])
+	{
+		printf("%s\n", new_arr[i]);
+		i++;
+	}
+	free(new_arr);
+	return (0);
+} */
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 20:58:47 by bravnar           #+#    #+#             */
-/*   Updated: 2023/10/09 15:59:22 by smuravye         ###   ########.fr       */
+/*   Updated: 2023/10/09 17:16:15 by smuravye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ void	ft_putendl_fd(char *s, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
