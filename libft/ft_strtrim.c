db2754e6e5427aaeabe27ff758bb0739ffcd3b12
Stanislav Muravyev <smuravye@c1r16s1.42lausanne.ch>
2023-10-09 16:00:05 +0200

    Pushing ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,23 +6,37 @@
 /*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:43:25 by smuravyev         #+#    #+#             */
-/*   Updated: 2023/10/09 15:13:42 by smuravye         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:58:20 by smuravye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	i;
+	size_t	j;
+	char	*substr;
+
+	i = 0;
+	j = ft_strlen(s1);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	if (j > 0)
+	{
+		while (s1[j - 1] && ft_strrchr(set, s1[j - 1]) && j > i)
+			j--;
+	}
+	substr = ft_substr(s1, i, j - i);
+	return (substr);
 }
 
 /* #include <stdio.h>
 
 int	main(void)
 {
-	char	*str = "pp01234567890pp";
-	char	*set = "p";
+	char	*str = "    pp01234567890pp    ";
+	char	*set = " ";
 	char	*trimmed_str = ft_strtrim(str, set);
 	printf("The trimmed string is: %s\n", trimmed_str);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 20:58:47 by bravnar           #+#    #+#             */
-/*   Updated: 2023/10/09 15:14:40 by smuravye         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:59:22 by smuravye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_putendl_fd(char *s, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
