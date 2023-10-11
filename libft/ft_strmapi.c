ba2d4751c33742e134509977df69ba8aa85696ed
Stanislav Muravyev <smuravye@c1r16s5.42lausanne.ch>
2023-10-11 10:00:15 +0200

    Pushing mapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 09:36:15 by smuravye          #+#    #+#             */
+/*   Updated: 2023/10/11 09:46:29 by smuravye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	length;
+	char	*result;
+	size_t	i;
+
+	length = ft_strlen(s);
+	result = malloc(sizeof(*s) * (length + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+}
+
+int	main(void)
