6f527422d4a9881f94107a2c003b9844a6885c6c
Stanislav Muravyev <smuravye@c2r4s5.42lausanne.ch>
2023-10-13 14:39:47 +0200

    pushing all /w strict

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 11:45:36 by smuravye          #+#    #+#             */
+/*   Updated: 2023/10/13 11:52:47 by smuravye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		current = *lst;
+		*lst = (*lst)->next;
+		del(current->content);
+		free(current);
+	}
+}
